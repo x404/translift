cf4fe05aa20a486113db5786a336865ea1a1bd99
x404 <kronos2003@gmail.com>
2018-02-14 14:30:34 +0200

    Добавлена валидация, телефонная маска

--- app/template/js/engine.js
+++ app/template/js/engine.js
@@ -40,8 +40,35 @@ $(document).ready(function(){
 	});	
 
 
+
+
+	// var thank = '<div class="thank text-center"><p>Спасибо за заказ продукта на нашем сайте. В ближайщее время с вами свяжутся наши менеджеры для уточнения всех деталей.</p></div>';
+	var thankcallback = '<div class="thank text-center"><p>В ближайщее время с вами свяжутся наши менеджеры для уточнения всех деталей</p></div>';
+	// var thankaddreview = '<div class="thank text-center"><p>Ваш отзыв отправлен!</p></div>';
+	// var thankreview = '<div class="thank text-center"><p>Спасибо за оставленный отзыв.</p></div>';
+	// var thankqorder = '<div class="thank text-center"><p>Спасибо за заказ продукта на нашем сайте. В ближайщее время с вами свяжутся наши менеджеры для уточнения всех деталей.</p></div>';
+	var errorTxt = 'Возникла ошибка при отправке заявки!';
+
+
 	// validation forms
+	$.validator.addMethod("validphone", function(value){
+		if (Inputmask.isValid(value, { mask: '+7(999)999-99-99'})) return true
+		else return false;
+	},"");
+
 	$('#callback-form').validate({
+		rules: {
+			name:{required : true},
+			tel: {validphone:true}
+		},
+		errorPlacement: function(error, element) {
+			if (element.attr('name') == 'name') {
+				$(element).addClass('error')
+			};
+			if (element.attr('name') == 'tel'){
+				$(element).addClass('error')
+			};
+		},
 		submitHandler: function(form){
 			var strSubmit=$(form).serialize();
 			$(form).find('fieldset').hide();
@@ -52,7 +79,7 @@ $(document).ready(function(){
 				data: strSubmit,
 				success: function(){
 					$(form).closest('.modal__body').html(thankcallback);
-					startClock('callback-form');
+					startClock('callback');
 				},
 				error: function(){
 					alert(errorTxt);
@@ -66,7 +93,11 @@ $(document).ready(function(){
 		}
 	});
 
-	
+	$('.tel').inputmask("+7(999)999-99-99");
+
+
+
+
 });
 
 // =заглушка для IE
@@ -111,3 +142,57 @@ $(function(){
 		}
 	})
 });
+
+
+
+
+var timer,
+	sec = 3;
+
+
+function showTime(sendform){
+	sec = sec-1;
+	if (sec <=0) {
+		stopClock();
+
+		switch (sendform){
+			case 'qorder-form':
+				$('.qorder__box .thank').fadeOut('normal',function(){
+					$('.qorder__box .thank').remove();
+					$('.qorder__box .form-control, .qorder__box textarea').val('');
+				});
+				break;
+			case 'feedback-form':
+				$('.feedback .thank').fadeOut('normal',function(){
+					$('.feedback .thank').remove();
+					$('.feedback .form-control, .feedback textarea').val('');
+					$('.feedback__form fieldset').show();
+				});
+				break;
+			case 'cart-form':
+				$('.cart .thank').fadeOut('normal',function(){
+					$('.cart .thank').remove();
+					// $('.cart .form-control, .cart textarea').val('');
+					// $('.cart__form fieldset').show();
+				});
+				break;	
+			default:
+			console.log(sendform);
+				modal = $("#" + sendform);
+				modal.fadeOut('normal',function(){
+					modal.find('.md-close').trigger('click');
+				});
+				break;
+		}
+	}
+}
+function stopClock(){
+	window.clearInterval(timer);
+	timer = null;
+	sec = 3;
+}
+
+function startClock(sendform){
+	if (!timer)
+		timer = window.setInterval("showTime('" + sendform + "')",1000);
+}

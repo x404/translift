6b33a2b4f8c9bcf0deeb4bcb7e178fa1184b4350
x404 <kronos2003@gmail.com>
2018-02-14 12:31:21 +0200

    Добавлен скрипт для модальных окон

---
+++ app/template/js/modalEffects.js
@@ -0,0 +1,69 @@
+/**
+ * modalEffects.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2013, Codrops
+ * http://www.codrops.com
+ */
+var ModalEffects = (function() {
+
+	function init() {
+
+		var overlay = document.querySelector( '.md-overlay' );
+
+		[].slice.call( document.querySelectorAll( '.md-trigger' ) ).forEach( function( el, i ) {
+
+			var modal = document.querySelector( '#' + el.getAttribute( 'data-modal' ) ),
+				close = modal.querySelector( '.md-close' );
+
+			function removeModal( hasPerspective ) {
+				classie.remove( modal, 'md-show' );
+				classie.remove( document.body, 'modal-open' );
+
+				if( hasPerspective ) {
+					classie.remove( document.documentElement, 'md-perspective' );
+				}
+			}
+
+			function removeModalHandler() {
+				removeModal( classie.has( el, 'md-setperspective' ) ); 
+			}
+
+			el.addEventListener( 'click', function( ev ) {
+				classie.add( modal, 'md-show' );
+				overlay.removeEventListener( 'click', removeModalHandler );
+				overlay.addEventListener( 'click', removeModalHandler );
+
+				classie.add( document.body, 'modal-open' );
+
+				if( classie.has( el, 'md-setperspective' ) ) {
+					setTimeout( function() {
+						classie.add( document.documentElement, 'md-perspective' );
+					}, 25 );
+				}
+			});
+
+			close.addEventListener( 'click', function( ev ) {
+				ev.stopPropagation();
+				removeModalHandler();
+			});
+
+		} );
+
+	}
+
+	init();
+
+})();
+
+$(document).keyup(function(e) {
+  if (e.keyCode == 27) {
+    if ($(".md-modal").hasClass('md-show')) {
+      console.debug('Pushed: "ESC"');
+      $('.md-modal').removeClass('md-show');
+    }
+  }
+});
--- gulpfile.js
+++ gulpfile.js
@@ -56,11 +56,6 @@ gulp.task('compress', function(){
 	pump([
 			gulp.src([  // Берем все необходимые библиотеки
 				config.libsDir + '/jquery/dist/jquery.js',
-				config.templateDir + '/js/util.js',
-				config.templateDir + '/js/modal.js',
-				config.templateDir + '/js/tab.js',
-				config.templateDir + '/js/popper.min.js',
-				config.templateDir + '/js/scrollspy.js',
 				config.libsDir + '/jquery-validation/dist/jquery.validate.js'
 			]),
 			concat('libs.min.js'), // Собираем их в кучу в новом файле libs.min.js
